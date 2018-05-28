b5d0c83b613c71d03f149a53bf0ab6bff13f9b4f
JUWON-KEVIN-LEE <quf9345@gmail.com>
2018-05-28 23:45:28 +0900

    feat: Added dependencies and resource files.

---
+++ app/src/main/java/com/kevin/newsapp/App.kt
@@ -0,0 +1,10 @@
+package com.kevin.newsapp
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/kevin/newsapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.kevin.newsapp.ui.splash
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kevin.newsapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
